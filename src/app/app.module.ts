8eb8b7ff19162ccfbee2d182089a682c48687965
mohamed <mox.meziani@mail.com>
2022-03-01 09:48:44 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,28 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBZ0mffimf9pfhC8q03gMWmytloVYeM6V8',
+  authDomain: 'airbnmap-91154.firebaseapp.com',
+  databaseURL: 'https://airbnmap-91154-default-rtdb.firebaseio.com',
+  projectId: 'airbnmap-91154',
+  storageBucket: 'airbnmap-91154.appspot.com',
+  messagingSenderId: '970770550135',
+  appId: '1:970770550135:web:5c0a682f081ae5dbe66572',
+  measurementId: 'G-RNH5YMC7Q3'
+};
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule,
+            IonicModule.forRoot(),
+            AppRoutingModule,
+            AngularFireModule.initializeApp(firebaseConfig),
+            AngularFireDatabaseModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+ showForm: boolean; // Variable pour afficher le formulaire (true) ou (false)
+ fireBaseData = { // Objet qui represente une ville
+   name:'',
+   img:'',
+   lat: '',
+   lng: ''
+ };
+ cities: Observable<any[]>; // Variable pour recuperer les villes dans la base de donnée firebase
 
-  constructor() {}
+  constructor(
+    public afDB: AngularFireDatabase
+  ) {}
 
+  ngOnInit(): void {
+    this.loadCities();
+
+  }
+
+
+  add(){ // Fonction pour afficher le formulaire
+  this.showForm = !this.showForm;
+  }
+
+  addFireBase(){
+    if(this.fireBaseData.name !== '' && this.fireBaseData.img !== '' && this.fireBaseData.lat !== '' && this.fireBaseData.lng !== ''){
+    this.afDB.list('City/').push(this.fireBaseData); // Ajout dans la base de donnée
+    this.add(); // On ferme le formulaire
+    this.fireBaseData = { // Ont vide les champs du formulaire
+      name:'',
+      img:'',
+      lat: '',
+      lng: ''
+    };
+  }
+  }
+
+
+  loadCities() {
+    this.cities = this.afDB.list('City/').valueChanges();
+  }
 }
+

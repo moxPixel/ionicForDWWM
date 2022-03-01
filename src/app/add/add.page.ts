c589e4b854b9c77a50c1906a3dc0cc08bb41411f
mohamed <mox.meziani@mail.com>
2022-03-01 17:00:33 +0100

    commit

--- src/app/add/add.page.ts
+++ src/app/add/add.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { ModalController, NavParams } from '@ionic/angular';
 
 @Component({
   selector: 'app-add',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add.page.scss'],
 })
 export class AddPage implements OnInit {
-
-  constructor() { }
+  cityDataLogement = {
+    lat: 0,
+    lng: 0,
+    price: '',
+    title: '',
+    type: '',
+    img: '',
+  };
+  constructor(
+    public afDB: AngularFireDatabase,
+    public modalController: ModalController,
+    public navParams: NavParams) {
+      this.cityDataLogement.lat =  this.navParams.get('lat');
+      this.cityDataLogement.lng =  this.navParams.get('lng');
+    }
 
   ngOnInit() {
   }
 
+
+  close(){
+    this.modalController.dismiss();
+  }
+
+  addFirebase() {
+    if (this.cityDataLogement.title !== '' &&
+     this.cityDataLogement.price !== '' &&
+      this.cityDataLogement.type !== '' &&
+       this.cityDataLogement.img !== '') { // Si les champs sont remplis.
+      this.afDB.list('Host/').push(this.cityDataLogement); // On ajoute le logement dans la base de données.
+      this.close();// On ferme la modal.
+    }
+
+  }
 }
--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
 import { ActivatedRoute } from '@angular/router';
-import { Environment, GoogleMap, GoogleMaps, GoogleMapsMapTypeId } from '@ionic-native/google-maps';
+import { Environment, GoogleMap, GoogleMaps, GoogleMapsEvent, GoogleMapsMapTypeId } from '@ionic-native/google-maps';
 import { ModalController, Platform } from '@ionic/angular';
+import { AddPage } from '../add/add.page';
 
 @Component({
   selector: 'app-map',
@@ -16,8 +17,15 @@ currentCity = {
   lng: 0
 };
 
-map: GoogleMap;
+showRoom = {
+  title:'',
+  img:'',
+  price:'',
+  type:'',
+};
 
+map: GoogleMap;
+markers: any;
   constructor(
     public afDB: AngularFireDatabase,
     private route: ActivatedRoute,
@@ -33,6 +41,7 @@ map: GoogleMap;
   ngOnInit() {
     if(this.platform.is('cordova')){
       this.loadMap(); // On charge la carte si on est sur un appareil
+      this.loadMarker(); // On charge les markers si on est sur un appareil
     }
 
   }
@@ -55,5 +64,86 @@ map: GoogleMap;
       }
     }); // On crée la carte
   }
+
+  async openAddPage(){
+     const modal = await this.modalController.create({
+       component:AddPage,
+        componentProps:{
+          lat: this.map.getCameraPosition().target.lat,
+          lng: this.map.getCameraPosition().target.lng,
+        }
+     });
+     return await modal.present();
+  }
+
+//Derniere partit du projet. On ajoute les markers.
+loadMarker() {
+  this.afDB.list('Host/').snapshotChanges(['child_added']).subscribe(datas => { // On recupere les données de la base de données.
+  this.map.clear(); // On efface les markers.
+    datas.forEach(element => { // On parcours les données.
+      // eslint-disable-next-line max-len
+      const markerIcon = this.createMarkerIcon(element.payload.exportVal().price, '#FF5A5F'); // On crée un marker personalisé.
+    this.markers =  this.map.addMarkerSync({ // On ajoute un marker.
+        animation: 'DROP',
+        icon: markerIcon,
+        position: { // On definit la position du marker.
+          lat: element.payload.exportVal().lat,
+          lng: element.payload.exportVal().lng
+        }
+      });
+      this.markers.addEventListener(GoogleMapsEvent.MARKER_CLICK).subscribe(() => { // on ajoute un evenement au marker.
+        this.showRoom.title = element.payload.exportVal().title; // On affecte le titre a l object showroom
+        this.showRoom.img = element.payload.exportVal().img; // On affecte l'image a l object showroom
+        this.showRoom.price = element.payload.exportVal().price; // On affecte le prix a l object showroom
+        this.showRoom.type = element.payload.exportVal().type; // On affecte le type a l object showroom
+      });
+    });
+  });
+}
+
+
+//  ont créé un marker personalisé.
+createMarkerIcon(price: number, color: string) {
+  const canvas = document.createElement('canvas'); // On crée un canvas.
+  canvas.width = 47; // On definit la largeur du canvas.
+  canvas.height = 39; // On definit la taille du canvas.
+  const rectangle = canvas.getContext('2d'); // On definit le contexte du canvas.
+  rectangle.beginPath();  // On definit le chemin du canvas.
+  rectangle.rect(2, 2, 43, 26); // On definit la taille du rectangle.
+  rectangle.fillStyle = color; // On definit la couleur du rectangle.
+  rectangle.lineWidth = 2; // On definit la largeur de la ligne.
+  rectangle.strokeStyle = '#666666'; // On definit la couleur de la ligne.
+  rectangle.stroke(); // On trace la ligne.
+  rectangle.fill(); // On remplit le rectangle.
+  const triangle = canvas.getContext('2d'); // On definit le contexte du canvas.
+  triangle.beginPath(); // On definit le chemin du canvas.
+  triangle.moveTo(12, 28); // On definit la position du triangle.
+  triangle.lineTo(32, 28); // On definit la position du triangle.
+  triangle.lineTo(22, 38); // On definit la position du triangle.
+  triangle.closePath(); // On definit la position du triangle.
+  triangle.fillStyle = color;
+  triangle.fill(); // On remplit le triangle.
+  const bord = canvas.getContext('2d');  // On definit le contexte du canvas.
+  bord.beginPath();  // On definit le chemin du canvas.
+  bord.moveTo(12, 28); // On definit la position du triangle.
+  bord.lineTo(22, 38); // On definit la position du triangle.
+  bord.lineTo(32, 28); // On definit la position du triangle.
+  bord.lineWidth = 1; // On definit la largeur de la ligne.
+  bord.strokeStyle = '#666666'; // On definit la couleur de la ligne.
+  bord.stroke(); // On trace la ligne.
+  const text = canvas.getContext('2d'); // On definit le contexte du canvas.
+  text.font = '11pt Arial'; // On definit la taille de la police.
+  text.fillStyle = 'white'; // On definit la couleur du texte.
+  if (price > 100) { // Si le prix est supérieur à 100.
+      text.fillText(price + '€', 7, 21); // On affiche le prix.
+  } else {
+      text.fillText(price + '€', 11, 21);
+  }
+  return canvas.toDataURL(); // On retourne le canvas.
+
+
+}
+
+
 
 }
